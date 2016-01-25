33d2e5d5792d0a57ce59c6c645b75e9d854de279
Neethu <neethu89@ccs.neu.edu>
2016-01-25 07:46:18 -0500

    test server root

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.get('/sayHello', rootRequest);
+
 app.listen(port, ipaddress);
 function rootRequest(req, res){
     res.send('hello world');
